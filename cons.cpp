ffc53412b544c74a2a7163de9de77d5203654583
K Adith Sivadasan <kadith65@gmail.com>
2026-03-28 23:23:39 +0530

    Add files via upload

---
+++ cons.cpp
@@ -0,0 +1,23 @@
+#include <iostream.h>
+#include <conio.h>
+class abc
+{
+public:
+int num1, num 2;
+float result;
+abc ()
+{
+cout <<" Constructor called";
+}
+void displaydata()
+{
+cout << "displaydata";
+}
+};
+
+void main()
+{
+.abc obj;
+obj. displaydata();
+getch;
+}
