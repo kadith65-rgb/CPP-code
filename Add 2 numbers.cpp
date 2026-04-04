80fa56185ff98dda7e64ccd08c4899b6f8800882
K Adith Sivadasan <kadith65@gmail.com>
2026-04-04 07:59:19 +0530

    Add files via upload

---
+++ Add 2 numbers.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+void main() {
+    int num1, num2, sum;
+    cout << "Enter first number: ";
+    cin >> num1;
+    cout << "Enter second number: ";
+    cin >> num2;
+    sum = num1 + num2;
+    cout << "Sum = " << sum ;
+}
